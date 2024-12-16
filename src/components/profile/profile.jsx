b1e770c590426bfcd4f3ba8551a3005f15b31199
Jinwei Ang <jinwei.ang@dxc.com>
2024-12-16 16:37:29 +0800

    Added responsive view

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './profile.css'
 import profile_img from '../../assets/profile_img.jfif'
 
-const Hero = () => {
+const profile = () => {
     return (
-        <div className='profile'>
+        <div className='profile' id='home'>
             <img src={profile_img} alt="JW"/>
             <h1><span>I'm Ang Jin Wei,</span> a Data Engineer / Aspiring Software Engineer based in Singapore.</h1>
             <p>I am current a Data Engineer @ Luxoft, recently graduated with a Bachelors of Information Technology, majoring in Computer Science and Business Information Systems.</p>
@@ -16,4 +16,4 @@ const Hero = () => {
     )
 }
 
-export default Hero
+export default profile
