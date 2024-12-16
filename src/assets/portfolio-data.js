b874d390a252b1fd6e5180d137df0df595c717a0
Jinwei Ang <jinwei.ang@dxc.com>
2024-12-16 17:22:35 +0800

    Added placeholder project images

--- src/assets/portfolio-data.js
+++ src/assets/portfolio-data.js
@@ -1,9 +1,9 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from '../assets/jwlogo.svg'
+import project2_img from '../assets/jwlogo.svg'
+import project3_img from '../assets/jwlogo.svg'
+import project4_img from '../assets/jwlogo.svg'
+import project5_img from '../assets/jwlogo.svg'
+import project6_img from '../assets/jwlogo.svg'
 
 const mywork_data = [
     {
