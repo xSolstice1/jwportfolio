cc6b4af1984a7b123bfc2b856b770dbb45342dd4
Jinwei Ang <jinwei.ang@dxc.com>
2024-12-16 14:03:13 +0800

    Added About Me Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Profile from './components/profile/profile'
+import About from './components/about/about'
 
 const app = () => {
   return (
     <div>
       <Navbar/>
       <Profile/>
+      <About/>
     </div>
   )
 }
---
+++ src/components/about/about.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './about.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/profile_img.jfif'
+
+const about = () => {
+  return (
+    <div className='about'>
+        <div className='about-title'>
+            <h1>About Me</h1>
+            <img src={theme_pattern} alt=""/>
+        </div>
+        <div className="about-sections">
+            <div className="about-left">
+                <img src={profile_img} alt=""/>
+            </div>
+            <div className="about-right">
+                <div className="about-para">
+                    <p>I am currently a Data Engineer @ Luxoft, building Data Pipelines and Analytical Dashboards for a prominent investment holdings.</p>
+                    <p>I am particularly interested in creating solutions for the masses and am currently aspiring to be a Software Engineer.</p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill">
+                        <p>Snowflake</p>
+                        <hr style={{width:"75%"}}/>
+                    </div>
+                    <div className="about-skill">
+                        <p>SQL</p>
+                        <hr style={{width:"80%"}}/>
+                    </div>
+                    <div className="about-skill">
+                        <p>Tableau</p>
+                        <hr style={{width:"40%"}}/>
+                    </div>
+                    <div className="about-skill">
+                        <p>C++</p>
+                        <hr style={{width:"75%"}}/>
+                    </div>
+                    <div className="about-skill">
+                        <p>Python</p>
+                        <hr style={{width:"60%"}}/>
+                    </div>
+                    <div className="about-skill">
+                        <p>React</p>
+                        <hr style={{width:"40%"}}/>
+                    </div>
+                    <div className="about-skill">
+                        <p>Javascript</p>
+                        <hr style={{width:"50%"}}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>2+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr/>
+            <div className="about-achievement">
+                <h1>5</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default about
